07314d8a9221c661d0160a362097c3c6858b88b3
An Phan <tqtthuyan71@gmail.com>
2025-09-08 12:28:31 +0930

    structure the folders using maven

---
+++ weather-aggregator/src/main/java/com/weather/client/getclient/getClient.java
@@ -0,0 +1,12 @@
+package getclient;
+
+public class getClient {
+    protected String host_name;
+    protected int port;
+
+    // constructor 
+    public getClient(String host_name, int port) {
+        this.host_name = host_name;
+        this.port = port;
+    }
+}
